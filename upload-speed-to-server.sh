8acec859363288a95dc528ca322854e4adbcfcbd
Cerem Cem ASLAN <cca@aktos.io>
2019-12-14 06:40:02 +0000

    masked dd statistics

--- upload-speed-to-server.sh
+++ upload-speed-to-server.sh
@@ -3,6 +3,6 @@ IP=$(curl ipecho.net/plain 2> /dev/null)
 PORT=${1:-9000}
 echo "Use the following command to upload the file to this server:"
 echo
-echo "    dd if=/dev/zero count=10000 | pv | nc $IP $PORT -q 1"
+echo "    dd if=/dev/zero count=10000 2> /dev/null | pv | nc $IP $PORT -q 1"
 echo
 nc -l -p $PORT | dd of=/dev/null 2> /dev/null
